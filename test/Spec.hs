c9fd260d0e8104ee875394cda84ca7f31d1bb049
Sam Delmerico <svdelmerico@gmail.com>
2019-08-14 20:29:47 -0400

    add first tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,18 +8,18 @@ import Syntax
 
 import Control.Monad.State.Lazy
 
+skkIdentity = "(\\x. (\\y. (\\z. x z (y z)))) (\\x. (\\y. x)) (\\x. (\\y . x))"
+
 main :: IO ()
 main = hspec $ do
     describe "parser" $ do
         it "can parse the SKK identity" $ do
-            let src = "(\\x. (\\y. (\\z. x z (y z)))) (\\x. (\\y. x)) (\\x. (\\y . x))"
-            property $ case parseExpr src of
+            property $ case parseExpr skkIdentity of
                 Right _ -> True
                 Left _ -> False
 
     describe "evaluator" $ do
         it "reduces the SKK identity" $ do
-            let src = "(\\x. (\\y. (\\z. x z (y z)))) (\\x. (\\y. x)) (\\x. (\\y . x))"
-            let (Right expr) = parseExpr src
+            let (Right expr) = parseExpr skkIdentity
             let reduced = evalState (reduce expr) defaultStack
             reduced `shouldBe` (Lam "z" (Var "z"))
